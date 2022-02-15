7170dbf686104b2b80bbc74ef859c872773e148e
TaeJoonPark <tjpark1028@gmail.com>
2022-02-15 14:40:44 +0900

    Front 초기세팅

---
+++ frontend/src/Router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function Router() {
+  <BrowserRouter>
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </BrowserRouter>;
+}
+
+export default Router;
