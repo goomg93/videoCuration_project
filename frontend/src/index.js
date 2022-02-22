62bf2e879cbad9e2ef68747b5d0713becd38d67f
ebchoi <eunbichoi1@gmail.com>
2022-02-22 17:52:01 +0900

    [Add] 디테일페이지 레이아웃 및 비디오 불러오기 설정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { Reset } from 'styled-reset';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://www2.wecode.buzzntrend.com/graphql',
+});
+
+const authLink = setContext(() => {
+  return { headers: { authorization: 1234 } };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Reset />
     <Router />
   </ApolloProvider>,
   document.getElementById('root')
