a616243284f6b9e7acde496f6eacaa4c513e1dfc
ebchoi <eunbichoi1@gmail.com>
2022-02-25 05:15:49 +0900

    [WIP] 비디오페이지네이션 쿼리문만 수정

--- frontend/src/pages/List/List.js
+++ frontend/src/pages/List/List.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Thumbnail from './ListComponent/Thumbnail';
 import styles from './List.module.css';
 
 const GET_LIST = gql`
-  query GetList {
-    videos {
+  query VideoPagination($index: Int!, $limit: Int!) {
+    videoPagination(index: $index, limit: $limit) {
+      id
       videoId
       thumbnails
     }
@@ -13,23 +14,26 @@ const GET_LIST = gql`
 `;
 
 function List() {
-  const [list, setList] = useState('');
-  const [listAmt, setListAmt] = useState(10);
+  const [hasMore, setHasMore] = useState(false);
+  const index = useRef(1);
+  const limit = 15;
 
-  const { loading, error, data, networkStatus } = useQuery(GET_LIST, {
-    variables: listAmt,
+  const { loading, error, data, fetchMore } = useQuery(GET_LIST, {
+    variables: { index: 1, limit: 15 },
     notifyOnNetworkStatusChange: true,
   });
-
   if (loading) return <p>Loading....</p>;
   if (error) return <p>Error To Render</p>;
-
-  console.log('networkStatus :', networkStatus);
-  console.log('dataLength : ', data?.videos.length);
-
+  if (data) {
+    const dataLength = data?.videoPagination.length;
+    index.current = index.current + dataLength;
+    if (dataLength < limit) {
+      setHasMore(false);
+    }
+  }
   return (
     <section className={styles.ListArea}>
-      {data?.videos.map((data, index) => (
+      {data?.videoPagination.map((data, index) => (
         <Thumbnail
           className={styles.Thumbnail}
           thumbnails={data.thumbnails}
