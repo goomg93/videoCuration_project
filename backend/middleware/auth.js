b4f5c6a19a2e6d7c066bd687df96a5b4f3e66a17
guminki <rnalsrl93@gmail.com>
2022-02-25 10:34:28 +0900

    [modify] 인증 로직 수정

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,34 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const authentication = context => {
+//   if (context.req.headers.origin === process.env.FRONTEND_URL) {
+//     return;
+//   } else if (context.req.headers.authorization === process.env.API_KEY) {
+//     context.setHeaders.push({
+//       key: 'Access-Control-Allow-Origin',
+//       value: context.req.headers.origin,
+//     });
+//   } else {
+//     context.setHeaders.push({
+//       key: 'Access-Control-Allow-Origin',
+//       value: null,
+//     });
+//     const err = new Error('INVALID ACCESS');
+//     throw err;
+//   }
+// };
 
 const authentication = context => {
-  if (context.req.headers.origin === process.env.FRONTEND_URL) {
+  if (context.req.headers.domain === 'http://localhost:3000/') {
     return;
   } else if (context.req.headers.authorization === process.env.API_KEY) {
-    context.setHeaders.push({
-      key: 'Access-Control-Allow-Origin',
-      value: context.req.headers.origin,
-    });
+    return;
   } else {
-    context.setHeaders.push({
-      key: 'Access-Control-Allow-Origin',
-      value: null,
-    });
     const err = new Error('INVALID ACCESS');
+    console.log('domain : ' + context.req.headers.domain);
+    console.log('origin : ' + context.req.headers.origin);
     throw err;
   }
 };
