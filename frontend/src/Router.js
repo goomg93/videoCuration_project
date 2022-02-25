5fd628fed84b1a7863d056dd4cd051e7cc64904f
ebchoi <eunbichoi1@gmail.com>
2022-02-25 11:42:25 +0900

    [Edit] 헤더 호버 색 맞춤

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './pages/Layout/Layout';
 import List from './pages/List/List';
+import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import DetailReact from './pages/DetailReact/DetailReact';
 import DetailLayer from './pages/DetailLayer/DetailLayer';
@@ -13,6 +14,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<List />} />
+          <Route path="/main" element={<Main />} />
           <Route path="/video/:videoId" element={<Detail />} />
           <Route path="/react/:videoId" element={<DetailReact />} />
           <Route path="/player/:videoId" element={<DetailLayer />} />
