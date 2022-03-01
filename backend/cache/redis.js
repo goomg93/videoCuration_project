5f885763e8d89bb116163e107fef57cdfaae77a3
LJHyeokk <clzlsqkq56@naver.com>
2022-03-01 14:56:50 +0900

    [modify]:be - connect redis

--- backend/cache/redis.js
+++ backend/cache/redis.js
@@ -1,7 +1,9 @@
 const redis = require('redis');
 
 const connectRedisServer = async () => {
-  const client = redis.createClient();
+  const client = redis.createClient({
+    url: 'redis://@redis:6379',
+  });
 
   client.on('error', () => {
     throw new Error('connected error');
