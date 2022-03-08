6c3a8cb8063f659ed17debc7cd491c571efdfe8e
guminki <rnalsrl93@gmail.com>
2022-03-08 13:45:41 +0900

    [modify] allow origin 처리 로직

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const authentication = context => {
-  if (context.req.headers.domain === 'http://localhost:3000/') {
-    return;
-  } else if (context.req.headers.authorization === process.env.API_KEY) {
-    return;
-=======
 import { AuthenticationError } from 'apollo-server-express';
 import { client } from '../mongodb/chatDataHandler';
 import dotenv from 'dotenv';
@@ -16,8 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const authentication = async req => {
-  if (req.headers.domain === process.env.FRONTEND_URL) {
-    return;
+  if (!process.env.ALLOW_ORIGIN.includes(req.headers.origin)) {
+    throw new AuthenticationError('NOT ALLOW ORIGIN');
   } else if (req.headers.authorization) {
     const API_KEY = req.headers.authorization;
     const col = await client.db('videoInfo').collection('listId').findOne({ API_KEY: API_KEY });
@@ -25,7 +14,6 @@ const authentication = async req => {
       throw new AuthenticationError('INVALID ACCESS');
     }
     return col.LIST_ID;
->>>>>>> master
   } else {
     throw new AuthenticationError('INVALID ACCESS');
   }
