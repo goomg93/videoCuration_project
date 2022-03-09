1fc6239b685b925d91c2dd51d77ac3ff256132cf
ebchoi <eunbichoi1@gmail.com>
2022-03-09 20:31:26 +0900

    [delete] 불필요한 파일 삭제

--- frontend/src/pages/Layout/Layout.js
+++ frontend/src/pages/Layout/Layout.js
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Category from '../List/ListComponent/Category';
-import { gql, useQuery } from '@apollo/client';
 import dataFetch from '../../hooks/useDataFetch';
 
 const Layout = () => {
