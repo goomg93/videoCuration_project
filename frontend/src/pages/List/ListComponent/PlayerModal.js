434a65ea909cd8101b2a4e3dfb61142fbb7e69f9
pinedelo <pinedelo30@gmail.com>
2022-03-10 01:56:20 +0900

    [Edit]: modal 사이즈에 맞게 크기 등 css 및 state 변경

--- frontend/src/pages/List/ListComponent/PlayerModal.js
+++ frontend/src/pages/List/ListComponent/PlayerModal.js
@@ -4,13 +4,13 @@ import { useQuery } from '@apollo/client';
 import YouTube from 'react-youtube';
 import styles from './PlayerModal.module.css';
 import * as gQuery from '../../../Global_Queries';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 
-function PlayerModal({ videoId, playerHandler }) {
+function PlayerModal({ videoId, playerHandler, ppreview, setPpreview }) {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const autoPlay = false;
-  const preview = useSelector(state => state.ListStates.preview);
+  // const preview = useSelector(state => state.ListStates.preview);
 
   const { loading, error, data } = useQuery(gQuery.GET_VIDEO_INFO, {
     variables: { videoId: videoId },
@@ -18,15 +18,16 @@ function PlayerModal({ videoId, playerHandler }) {
   });
 
   if (loading) {
-    return <p>Keep Hovering On Player</p>;
+    return <p className={styles.LoadingAndError} />;
   }
   if (error) {
-    return <p>error to render....</p>;
+    return console.log(error);
+    // return <p className={styles.LoadingAndError}>error to render....</p>;
   }
 
   const opts = {
-    width: 304,
-    height: 171,
+    width: 288,
+    height: 162,
     playerVars: {
       // autoPlay => true: 자동재생
       // disablekb => true: 플레이어 컨트롤 block
@@ -41,7 +42,8 @@ function PlayerModal({ videoId, playerHandler }) {
 
   function playerOnReady(e) {
     if (e.target.getPlayerState() === -1) {
-      return dispatch({ type: 'PlayerModal/setPreview' });
+      // return dispatch({ type: 'PlayerModal/setPreview', payload: false });
+      return setPpreview(false);
     }
 
     return e.target.playVideo();
@@ -62,7 +64,7 @@ function PlayerModal({ videoId, playerHandler }) {
           onReady={playerOnReady}
         />
       </section>
-      {preview ? (
+      {ppreview ? (
         <section
           className={styles.CoverVideo}
           onClick={() => goToDetail(videoId)}
@@ -70,8 +72,12 @@ function PlayerModal({ videoId, playerHandler }) {
         />
       ) : (
         <section className={styles.BlockVideo} onMouseLeave={playerHandler}>
-          <p>현재 페이지에서 재생할 수 없는 영상입니다.</p>
-          <p>영상을 보고싶으시다면 하단에 링크를 클릭해주세요.</p>
+          <p className={styles.Block}>
+            해당페이지에서 재생이 불가능한 영상입니다.
+          </p>
+          <p className={styles.SecondBlock}>
+            재생을 원하시면 아래 링크를 클릭해주세요.
+          </p>
           <a href={`https://youtube.com/watch?v=${videoId}`}>Go To watch!</a>
         </section>
       )}
--- frontend/src/pages/List/ListComponent/Thumbnail.js
+++ frontend/src/pages/List/ListComponent/Thumbnail.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
 import PlayerModal from './PlayerModal';
 import styles from './Thumbnail.module.css';
 
 function Thumbnail({ title, thumbnails, videoId }) {
+  // const dispatch = useDispatch();
   const [player, setPlayer] = useState(false);
+  const [ppreview, setPpreview] = useState(true);
 
   const playerHandler = () => {
+    // dispatch({ type: 'PlayerModal/setPreview', payload: true });
+    setPpreview(true);
     return setPlayer(!player);
   };
 
@@ -18,9 +23,14 @@ function Thumbnail({ title, thumbnails, videoId }) {
           alt="thumbnails"
           onMouseEnter={playerHandler}
         />
-        <p>{title}</p>
+        <p className={styles.Title}>{title}</p>
         {player ? (
-          <PlayerModal videoId={videoId} playerHandler={playerHandler} />
+          <PlayerModal
+            videoId={videoId}
+            playerHandler={playerHandler}
+            ppreview={ppreview}
+            setPpreview={setPpreview}
+          />
         ) : null}
       </section>
     </section>
